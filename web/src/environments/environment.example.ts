7c4d27e15f147d6a2b01530dd2587069d7ce5c6b
Marcello Cosentino <cosentino.m2@2mlab.com>
2018-12-25 01:21:21 +0100

    Fixed package.json files: build task now builds for prod environment

--- web/src/environments/environment.example.ts
+++ web/src/environments/environment.example.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "<your-apiKey>",
-    authDomain: "<your-authDomain>",
-    databaseURL: "<your-databaseURL>",
-    projectId: "<your-projectId>",
-    storageBucket: "<your-storageBucket>",
-    messagingSenderId: "<your-messagingSenderId>",
+    apiKey: '<your-apiKey>',
+    authDomain: '<your-authDomain>',
+    databaseURL: '<your-databaseURL>',
+    projectId: '<your-projectId>',
+    storageBucket: '<your-storageBucket>',
+    messagingSenderId: '<your-messagingSenderId>',
   },
 };
 
